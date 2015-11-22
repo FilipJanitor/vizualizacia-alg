0974a63eaa929b249de71705e8a9b51ef6fa7e54
FilipJanitor <filip.janitor@gmail.com>
2015-11-22 17:05:51 +0100

    Opravene drobne chybicky s validnostou a preklepom

--- vizualizacia-alg/mainController.js
+++ vizualizacia-alg/mainController.js
@@ -20,7 +20,7 @@ app.controller('mainController', function($scope) {
 	$scope.kNotValid = function(value) {
 		return ($value > 0) && ($value<=30);
 	};
-	/*funkcia volana z formularu nastavujuceho k. Nastavi k, inicializuje vstupne pasky a nastavy dalsi STATE*/
+	/*funkcia volana z formularu nastavujuceho k. Nastavi k, inicializuje vstupne pasky a nastavi dalsi STATE*/
 	$scope.setKMode = function(mode,kValue) {
 			// $scope.k = kValue;
 			for (var i = 0; i < kNumber; i++) {
@@ -32,8 +32,8 @@ app.controller('mainController', function($scope) {
 				$scope.currentState = $scope.stateEnum.MODE_2_CHOOSE_INPUT;
 			}
 	};
-	/*Funkcia volana v stavoch CHOOSE. Pre value 1 yacne simulaciu modu 1. Pre 2 skoci do DELTA, pre 3 zacne simulaciu MODU 2 */
-	$scope.startSimulation function(value){
+	/*Funkcia volana v stavoch CHOOSE. Pre value 1 zacne simulaciu modu 1. Pre 2 skoci do DELTA, pre 3 zacne simulaciu MODU 2 */
+	$scope.startSimulation = function(value){
 		if(value == 1){
 			$scope.currentState = $scope.stateEnum.MODE_1_SIMULATE;
 		} else if(value == 2) {
