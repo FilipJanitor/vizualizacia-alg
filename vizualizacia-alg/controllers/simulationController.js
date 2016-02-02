5c5cc1cf6ef37c89597f1549bd011853f350d220
FilipJanitor <filip.janitor@gmail.com>
2016-02-02 21:59:06 +0100

    Pridane vykreslovanie origin. pasok

--- vizualizacia-alg/controllers/simulationController.js
+++ vizualizacia-alg/controllers/simulationController.js
@@ -32,8 +32,9 @@ app.controller('simulationController', ['$scope', '$window', 'simulationService'
         MODE_1_SIMULATE: 5,
         MODE_2_SIMULATE: 6,
     };
-    /*Pomocne polia na spravne vykreslenie*/
-
+   /*Pomocne polia na spravne vykreslenie*/
+    /*pole, ktore sa nebude menit a bude sluzit len na iterovanie cez view na paskach*/
+	 $scope.CONSTANT_VIEW_ARRAY = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
 
     /*Objekty s datami, ktore su odoslane cez service z maincontrolleru. Obsahuju presne to, co aj tam - aj nazvy su rovnake*/
     
