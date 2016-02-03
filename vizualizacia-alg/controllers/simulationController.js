200a0fdc63351bc730772206a2d83b6756deec92
FilipJanitor <filip.janitor@gmail.com>
2016-02-03 11:45:50 +0100

    Opravene konstanty pri vykreslovani orig. stroja

--- vizualizacia-alg/controllers/simulationController.js
+++ vizualizacia-alg/controllers/simulationController.js
@@ -34,7 +34,8 @@ app.controller('simulationController', ['$scope', '$window', 'simulationService'
     };
    /*Pomocne polia na spravne vykreslenie*/
     /*pole, ktore sa nebude menit a bude sluzit len na iterovanie cez view na paskach*/
-	 $scope.CONSTANT_VIEW_ARRAY = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19];
+	 $scope.CONSTANT_VIEW_ARRAY = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+	 $scope.CONSTANT_X_VIEW_ARRAY = [60,100,140,180,220,260,300,340,420,460,500,540,580,620,660,700];
 
     /*Objekty s datami, ktore su odoslane cez service z maincontrolleru. Obsahuju presne to, co aj tam - aj nazvy su rovnake*/
     
